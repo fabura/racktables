d5f5ac079e3b1784bbff137f0a3ece4f10e5799f
Bulat Fattahov <bulat.fattahov@inn.ru>
2013-06-17 20:19:04 +0400

    + Some cosmetic changes.

--- wwwroot/codeigniter/application/views/pages/taghistory.php
+++ wwwroot/codeigniter/application/views/pages/taghistory.php
@@ -13,7 +13,8 @@
                         <th ng-click="sort('asset_no')">Asset</th>
                     </tr>
                     </thead>
-                    <tr ng-repeat="elem in tagHistory | orderBy:orderBy:reverse">
+                    <tr ng-repeat="elem in tagHistory | orderBy:orderBy:reverse"
+                        class="{{ elem.operation == '+' && 'row_plus' || 'row_minus'  }}">
                         <td>
                             {{ elem.name }}
                         </td>
@@ -33,26 +34,50 @@
             </div>
             <div class="filters">
                 <form action="/index.php?page=reports&tab=taghistory" method="POST">
-                        <div id="tab">
-                        <h2 align="center">Tags</h2>
-                            <div id="tag_tree" class="filter" ng-controller="TagTreeCtrl">
-                            <div ng-repeat="rootNode in tagTree" class="separator">
-                                <div class="root-node  selected-{{rootNode.checked}}"><input type="checkbox" name="tag[{{rootNode.id}}]" ng-change="check(rootNode.id)"
-                                       ng-model="rootNode.checked">
+                    <input type="submit" id="btnSubmit" value="Filter!">
+                    <div id="tags_filter_tab" class="filter_panel">
+                        <div id="filter_buttons">
+                            <h2 align="center">Date Range</h2>
 
-                                {{rootNode.tag}}</div>
-                                <div ng-repeat="leaf in rootNode.kids " class="leaf-node selected-{{leaf.checked}}">
-                                    <div><input type="checkbox" name="tag[{{leaf.id}}]" ng-model="leaf.checked">
-                                    {{leaf.tag }}</div>
+                            <div id="date_filter" class="filter" ng-controller="DateCtrl">
+                                <p><b>From:</b> <input type="text" name="dateFrom"
+                                                       value= <?php echo "2000-01-01"; ?> id="dateFrom">
+                                </p>
+
+                                <p><b>To:</b> <input type="text" name="dateTo" value= <?php echo date('Y-m-d'); ?> id="dateTo"/>
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+
+                            <h2 align="center">Tags</h2>
+
+                            <div id="tag_tree" class="filter" ng-controller="TagTreeCtrl">
+                                <div ng-repeat="rootNode in tagTree" class="separator">
+                                    <div class="root-node  selected-{{rootNode.checked}}"><input type="checkbox"
+                                                                                                 name="tag[{{rootNode.id}}]"
+                                                                                                 ng-change="check(rootNode.id)"
+                                                                                                 ng-model="rootNode.checked">
+
+                                        {{rootNode.tag}}
+                                    </div>
+                                    <div ng-repeat="leaf in rootNode.kids " class="leaf-node selected-{{leaf.checked}}">
+                                        <div><input type="checkbox" name="tag[{{leaf.id}}]" ng-model="leaf.checked">
+                                            {{leaf.tag }}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div id="tab">
+                    <div id="object_filter_tab" class="filter_panel">
                         <h2 align="center">Objects</h2>
+
                         <div id="object_filter" class="filter" ng-controller="ObjectsCtrl">
                             Filter : <input name="query" ng-model="query">
-                            <div><input name="forall" type="checkbox" ng-model="forall" ng-change="toggle()">For all</div>
+
+                            <div><input name="forall" type="checkbox" ng-model="forall" ng-change="toggle()">For all
+                            </div>
                             <table>
                                 <thead>
                                 <tr>
@@ -62,7 +87,8 @@
                                 </tr>
                                 </thead>
                                 <tr ng-repeat="object in objects | search: query">
-                                    <td><input type="checkbox" ng-model="object.checked" name="object[{{object.id}}]"></td>
+                                    <td><input type="checkbox" ng-model="object.checked" name="object[{{object.id}}]">
+                                    </td>
                                     <td>{{object.name}}</td>
                                     <td ng-show="rack(object.id)">
                                         <a href="/index.php?page=row&row_id={{rack(object.id).row_id}}">{{rack(object.id).row_name}}</a>/
@@ -70,15 +96,6 @@
                                     </td>
                                 </tr>
                             </table>
-                        </div>
-                    </div>
-                        <div id="filter_buttons" class="filter">
-                            <h2 align="center">Date Range</h2>
-                            <div id="date_filter" class="filter" ng-controller="DateCtrl">
-                                <p><b>From:</b> <input type="text" name="dateFrom" value= <?php echo "0000-00-00";?> id="dateFrom"></p>
-                                <p><b>To:</b> <input type="text" name="dateTo" value= <?php echo date('Y-m-d');?> id="dateTo"/></p>
-                            </div>
-                            <input type="submit" id="btnSubmit" value="Отправить!">
                         </div>
                     </div>
                 </form>
@@ -92,7 +109,8 @@
     arrays.objects = <?php echo json_encode($objects);?>;
     arrays.tagHistory = <?php echo json_encode($taghistory); ?>;
     arrays.mountinfo = <?php echo json_encode($mountinfo); ?>;
-    arrays.filtervalues = <?php echo json_encode($filtervalues);?>
+    arrays.filtervalues =
+    <?php echo json_encode($filtervalues);?>
 </script>
 <script type="text/javascript" src="/js/additional/angular.js"></script>
 <script type="text/javascript" src="/js/additional/controllers.js"></script>
